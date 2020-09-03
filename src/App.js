a946e7ed1df56adde1cc60a076582d3dfff2aa24
Kai Patel <00kaipatel@gmail.com>
2020-09-03 15:51:56 +0100

    Added onclick removal

--- src/App.js
+++ src/App.js
@@ -8,23 +8,42 @@ class App extends React.Component {
         {
             todoText: "1",
             complete: false,
+            id: 0,
         },
         {
             todoText: "2",
             complete: false,
+            id: 1,
         },
         {
             todoText: "3",
             complete: true,
+            id: 2,
         },
     ];
 
     state = { todos: this.initialTodos };
 
+    onClick = (key) => {
+        let currentTodos = this.state.todos;
+
+        for (let i = 0; i < currentTodos.length; i++) {
+            if (currentTodos[i].id === key) {
+                currentTodos.splice(i, 1);
+            }
+        }
+
+        this.setState({ todos: currentTodos });
+        console.log(currentTodos);
+    };
+
     render() {
         return (
             <div className="root">
-                <TodoList todoState={this.state}></TodoList>
+                <TodoList
+                    click={this.onClick}
+                    todoState={this.state}
+                ></TodoList>
             </div>
         );
     }
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./todoitem.module.css"
+
 const TodoItem = (props) => {
-    return <ul className={`${styles.todoitem} ${props.todo.complete ? styles.complete : styles.incomplete}`}>{props.todo.todoText}</ul>;
+    return <ul onClick={() => props.click(props.todo.id)} className={`${styles.todoitem} ${props.todo.complete ? styles.complete : styles.incomplete}`}>{props.todo.todoText}</ul>;
 };
 
 export default TodoItem;
